dadcad2000919de632d7c93cef64ab09b8f0cc84
LuTsungTa <steven82813@gmail.com>
2019-07-02 15:25:52 +0800

    update! you can set up your Number

--- guessNum.py
+++ guessNum.py
@@ -5,19 +5,35 @@
 
 import random
 
-ans = random.randint(1, 100)
-attemptTimes = 0
+firstNum = input('請輸入最小數字： ')
+lastNum = input('請輸入最大數字： ')
+firstNum = int(firstNum)
+lastNum = int(lastNum)
 
 while True:
-	gusNum = input('請輸入要猜測的數字，介於1～100內： ')
-	gusNum = int(gusNum)
-	attemptTimes = attemptTimes + 1
-	if gusNum == ans:
-		print('終於猜對了！你總共嘗試了', attemptTimes,'次')
+	if firstNum > lastNum:
+		print('最小數字大於最大數字，請重新輸入')
+		break
+	elif firstNum < lastNum:
+		ans = random.randint(firstNum, lastNum)
+		attemptTimes = 0
+		while True:
+			# gusNum = input('請輸入要猜測的數字，數字介於', firstNum, '到', lastNum, '之間')
+			print('提示：數字介於', firstNum, '跟', lastNum, '之間')
+			gusNum = input('請輸入要猜測的數字')
+			gusNum = int(gusNum)
+			attemptTimes = attemptTimes + 1
+			if gusNum == ans:
+				print('終於猜對了！你總共嘗試了', attemptTimes,'次，遊戲結束！')
+				break
+			else:
+				print('猜錯了')
+				if gusNum > ans:
+					print('你猜的數字比較大')
+				elif gusNum < ans:
+					print('你猜的數字比較小')
 		break
 	else:
-		print('猜錯了')
-		if gusNum > ans:
-			print('你猜的數字比較大')
-		elif gusNum < ans:
-			print('你猜的數字比較小')
+		print('最小數字等於最大數字，請重新輸入')
+		break
+
